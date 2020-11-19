33269faf45676723ad9583a767c56fcfbf46ba17
Andrew Persad <akpersad@gmail.com>
2020-11-19 00:07:17 -0500

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./styles/main.scss";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import AppContainer from "./components/appContainer/appContainer";
+
+class App extends Component {
+	render() {
+		return (
+			<>
+				<AppContainer />
+			</>
+		);
+	}
+}
+
+export default App;
