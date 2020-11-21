8241724e25d0ef1c1e0227d969929e5475ad07b7
Andrew Persad <akpersad@gmail.com>
2020-11-21 18:42:05 -0500

    Styling

---
+++ src/components/pokedexLeftPanel/pokedexLeftPanel.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class PokedexLeftPanel extends Component {
+	render() {
+		return <div>PokedexLeftPanel</div>;
+	}
+}
+
+PokedexLeftPanel.propTypes = {};
+
+export default PokedexLeftPanel;
---
+++ src/components/pokedexRightPanel/pokedexRightPanel.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class PokedexRightPanel extends Component {
+	render() {
+		return <div>PokedexRightPanel</div>;
+	}
+}
+
+PokedexRightPanel.propTypes = {};
+
+export default PokedexRightPanel;
