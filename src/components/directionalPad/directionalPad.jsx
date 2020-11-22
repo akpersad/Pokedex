ff9f44fe2eb20059215aaf9f28b96b2d499b86ae
Andrew Persad <akpersad@gmail.com>
2020-11-22 18:01:34 -0500

    Implement move up and down the list

--- src/components/directionalPad/directionalPad.jsx
+++ src/components/directionalPad/directionalPad.jsx
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import store from "../../config/store";
 import ArrowUp from "../../global/assets/images/arrow-up.svg";
 import ArrowRight from "../../global/assets/images/arrow-right.svg";
 import ArrowDown from "../../global/assets/images/arrow-down.svg";
 import ArrowLeft from "../../global/assets/images/arrow-left.svg";
 import { fetchData } from "../pokemon/fetchPokemonData";
+import { pokemonNumer } from "../../global/_constants";
 
 class DirectionalPad extends Component {
 	handleBtnClick(event) {
 		const { pokemon } = store.getState();
 		const { pokemonInfo } = pokemon;
+		let counter = 0;
 
-		debugger;
+		switch (event.currentTarget.value) {
+			case "up":
+				counter = pokemonInfo.pokemonNumber + 10;
+				break;
+
+			case "right":
+				counter = pokemonInfo.pokemonNumber + 1;
+				break;
+
+			case "down":
+				counter = pokemonInfo.pokemonNumber - 10;
+				break;
+
+			case "left":
+				counter = pokemonInfo.pokemonNumber - 1;
+				break;
+
+			default:
+				break;
+		}
+
+		if (counter > 0 && counter < pokemonNumer + 1) {
+			pokemon.pokeLoad = true;
+
+			store.dispatch({
+				type: "START_LOAD",
+				payload: pokemon
+			});
+
+			pokemonInfo.pokemonNumber = counter;
+			fetchData();
+		}
 	}
 
 	render() {
@@ -95,7 +127,5 @@ class DirectionalPad extends Component {
 		);
 	}
 }
-
-DirectionalPad.propTypes = {};
 
 export default DirectionalPad;
