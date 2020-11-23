9335dc9337f8d318c9ed60f79aeb36c4f391e4da
Andrew Persad <akpersad@gmail.com>
2020-11-22 20:33:54 -0500

    Fetch random Pokemon

--- src/components/randomBtn/randomBtn.jsx
+++ src/components/randomBtn/randomBtn.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import store from "../../config/store";
+import { fetchData } from "../pokemon/fetchPokemonData";
+import { pokemonNumber } from "../../global/_constants";
 
 class RandomBtn extends Component {
+	dispatchRandomNum() {
+		const { pokemon } = store.getState();
+
+		pokemon.pokeLoad = true;
+
+		store.dispatch({
+			type: "START_LOAD",
+			payload: pokemon
+		});
+
+		const min = 1;
+		const intNumber = Math.floor(Math.random() * (pokemonNumber - min)) + min;
+		pokemon.pokemonInfo.pokemonNumber = intNumber;
+
+		fetchData();
+	}
+
 	render() {
 		return (
-			<button type="button" className="rand-button">
+			<button
+				type="button"
+				onClick={this.dispatchRandomNum}
+				className="rand-button"
+				title="Random Pokemon"
+			>
 				<span />
 			</button>
 		);
 	}
 }
-
-RandomBtn.propTypes = {};
 
 export default RandomBtn;
