62f0cf0aa987b92bcf763fc4162ae716473da1ca
Andrew Persad <akpersad@gmail.com>
2020-11-22 21:23:28 -0500

    Spinner for types

--- src/components/Types/Types.jsx
+++ src/components/Types/Types.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import Loader from "react-loader-spinner";
 
 class Types extends Component {
 	renderTypeList() {
@@ -17,10 +18,15 @@ class Types extends Component {
 	}
 
 	render() {
+		const { pokeLoad } = this.props;
 		return (
 			<div className="screen type-list">
 				<div className="panel-header">Types</div>
-				<div className="type-box">{this.renderTypeList()}</div>
+				{pokeLoad ? (
+					<Loader type="Oval" color="#00BFFF" height={150} width={150} />
+				) : (
+					<div className="type-box">{this.renderTypeList()}</div>
+				)}
 			</div>
 		);
 	}
@@ -28,7 +34,8 @@ class Types extends Component {
 
 Types.propTypes = {
 	pokemonInfo: PropTypes.object.isRequired,
-	pokemonType: PropTypes.array.isRequired
+	pokemonType: PropTypes.array.isRequired,
+	pokeLoad: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => {
