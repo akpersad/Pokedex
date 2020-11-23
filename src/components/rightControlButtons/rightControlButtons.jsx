9d509aa4cd1d339f6061a121c13504b529adf649
Andrew Persad <akpersad@gmail.com>
2020-11-22 19:55:08 -0500

    Implement toggle shiny functionality

--- src/components/rightControlButtons/rightControlButtons.jsx
+++ src/components/rightControlButtons/rightControlButtons.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import DirectionalPad from "../directionalPad/directionalPad";
+import ShinySelect from "../shinySelect/shinySelect";
 
 class RightControlButtons extends Component {
 	render() {
 		return (
 			<div className="control-btn_container">
 				<DirectionalPad />
+				<ShinySelect />
 			</div>
 		);
 	}
